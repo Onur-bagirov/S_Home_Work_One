9bded1c43449ce7863b1abe214a38386aaebc3e5
Onur <onurrbagir2008@gmail.com>
2025-09-16 17:36:03 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Thread Time = new Thread(() =>
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write($"\rTime: {DateTime.Now.ToLongTimeString()}   ");
+    Console.ResetColor();
+    Thread.Sleep(1000);
+});
+
+Thread Loading = new Thread(() =>
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.Write($"\n\t Loading... ");
+    Console.ResetColor();
+    Thread.Sleep(300);
+});
+
+Thread Input = new Thread(() =>
+{
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.Write("Enter name : ");
+    Console.ResetColor();
+    string Input = Console.ReadLine();
+
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine(Input);
+    Console.ResetColor();
+});
+
+Time.Start();
+Loading.Start();
+Input.Start();
